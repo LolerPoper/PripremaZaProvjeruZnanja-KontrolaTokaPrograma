a5d7450ff07a4280e95738c9a0296d9ed2158b80
LolerPoper <113007900+LolerPoper@users.noreply.github.com>
2023-01-18 08:53:13 +0100

    Add files via upload

---
+++ PravokutniTrokut/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PravokutniTrokut
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+
+            Console.WriteLine("Unesite 3 stranice pravokutnog trokuta, zadnja treba biti hipotenuza: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+            z = Convert.ToInt32(Console.ReadLine());
+
+            if ((x * x) + (y * y) == (z * z) & x != 0 & y != 0 & z != 0)
+            {
+                Console.WriteLine("Ovo je pravokutni trokut.");
+            }
+            else
+            {
+                Console.WriteLine("Ovo nije pravokutni trokut.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
