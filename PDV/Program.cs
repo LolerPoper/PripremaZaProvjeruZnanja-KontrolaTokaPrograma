d80c998570954913b1cd46c2aa78f45015db51d2
LolerPoper <113007900+LolerPoper@users.noreply.github.com>
2023-01-18 09:13:04 +0100

    Add files via upload

---
+++ PDV/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDV
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double cijena, cijenaPDV, PDV;
+
+            Console.WriteLine("Unesite cijenu bez i sa PDV-om: ");
+            cijena = Convert.ToDouble(Console.ReadLine());
+            cijenaPDV = Convert.ToDouble(Console.ReadLine());
+
+            PDV = (cijenaPDV - cijena) / cijena * 100;
+
+            Console.WriteLine("PDV je " + PDV + "%");
+
+            Console.ReadKey();
+        }
+    }
+}
