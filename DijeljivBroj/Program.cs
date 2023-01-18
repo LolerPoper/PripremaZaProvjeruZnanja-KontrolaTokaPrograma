eb8d08a2915fd24d7b9eae8fe25f4d0aea25fb31
LolerPoper <113007900+LolerPoper@users.noreply.github.com>
2023-01-18 09:01:35 +0100

    Add files via upload

---
+++ DijeljivBroj/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DijeljivBroj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double broj;
+
+            Console.WriteLine("Unesite broj: ");
+            broj = Convert.ToDouble(Console.ReadLine());
+
+            if(broj % 4 == 0 & broj % 6 !=0)
+            {
+                Console.WriteLine("Broj je dijeljiv s 4");
+            }
+            else if(broj % 4 != 0 & broj % 6 == 0)
+            {
+                Console.WriteLine("Broj je dijeljiv s 6");
+            }
+            else if (broj % 4 == 0 & broj % 6 == 0)
+            {
+                Console.WriteLine("Broj je dijeljiv sa 4 i sa 6");
+            }
+            else
+            {
+                Console.WriteLine("Broj nije dijeljiv sa 4 ni sa 6");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
